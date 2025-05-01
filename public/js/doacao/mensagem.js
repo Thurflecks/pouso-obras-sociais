d0a81ecb3c74682b5632b2530551c71a8cd24dee
Thurflecks <thurflecks.dev@gmail.com>
2025-05-01 14:23:04 -0300

    erros resolvidos, pagina doacao completa(front e back), alguma imagens apagadas, ajustes css

---
+++ public/js/doacao/mensagem.js
@@ -0,0 +1,24 @@
+function enviarMensagem(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nomeOutrasDoacoes").value;
+    const mensagem = document.getElementById("mensagem").value;
+    const telefoneOutrasDoacoes = document.getElementById("telefoneOutrasDoacoes").value;
+    const endereco = document.getElementById("endereco").value;
+    const dataDisponibilidade = document.getElementById("data").value;
+
+    const dataHora = new Date(dataDisponibilidade);
+    const dataFormatada = `${dataHora.toLocaleDateString('pt-BR')} às ${dataHora.toLocaleTimeString('pt-BR')}`;
+
+
+    const numero = "5598981212573";
+    const texto = `Nome: ${nome}, Mensagem: ${mensagem}, telefone: ${telefoneOutrasDoacoes}, endereço: ${endereco}, data e hora de disponibilidade: ${dataFormatada}`;
+
+    const msgFormatada = encodeURIComponent(texto)
+    const url = `https://wa.me/${numero}?text=${msgFormatada}`;
+    console.log(url);
+    window.open(url, '_blank');
+
+    document.getElementById('formulario-outras-doacoes').submit();
+
+}
